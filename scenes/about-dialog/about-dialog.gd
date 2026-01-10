5eeba906834a6d51000520e256609d40b99c117e
graydwarf <graydwarf@gmail.com>
2026-01-10 15:55:59 -0800

    Fix FluentUI license not loading in exported builds
    Embed license text directly in script instead of loading from external
    .txt file. Godot exports don't include .txt files by default.

--- scenes/about-dialog/about-dialog.gd
+++ scenes/about-dialog/about-dialog.gd
@@ -2,6 +2,28 @@ extends Panel
 
 @onready var _licenseTextEdit = $CenterContainer/Panel/VBoxContainer/ScrollContainer/MarginContainer2/CardsVBox/LicensesCard/VBoxContainer/LicenseTextEdit
 
+const FLUENT_UI_LICENSE = """MIT License
+
+Copyright (c) 2020 Microsoft Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
+
 func _ready():
 	LoadLicenseText()
 	LoadTheme()
@@ -10,23 +32,13 @@ func LoadTheme():
 	theme = load(App.GetThemePath())
 
 func LoadLicenseText():
-	var licensePath = "res://scenes/file-tree-view-explorer/assets/fluent-icons/FLUENT_UI_LICENSE.txt"
-	var file = FileAccess.open(licensePath, FileAccess.READ)
-
-	if file:
-		var licenseText = file.get_as_text()
-		file.close()
-
-		var fullText = """Godot Valet uses icons from Microsoft's Fluent UI System Icons library.
+	var fullText = """Godot Valet uses icons from Microsoft's Fluent UI System Icons library.
 
 ====================================================================
 
 """
-		fullText += licenseText
-
-		_licenseTextEdit.text = fullText
-	else:
-		_licenseTextEdit.text = "Error: Unable to load license file at " + licensePath
+	fullText += FLUENT_UI_LICENSE
+	_licenseTextEdit.text = fullText
 
 func _on_close_button_pressed():
 	queue_free()
